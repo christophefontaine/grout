3427c46e5149705a0bc14f132d84a65fab04ec40
Christophe Fontaine <cfontain@redhat.com>
2024-12-01 09:28:48 +0000

    infra: add generic mbuf priv data
    Some metadata are relevant for all nodes, such as the iface.
    Modify the gr_mbuf structure layout to prepend a generic
    set of metadata, in our case, a pointer to the interface.
    
    This pointer was present in most of the existing metadata,
    either as an "input_iface" (just named "iface"), or "output_iface".
    Rename it, and use it either as input or output iface accordingly.
    
    Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

--- modules/ip/datapath/gr_ip4_datapath.h
+++ modules/ip/datapath/gr_ip4_datapath.h
@@ -17,7 +17,6 @@
 #include <stdint.h>
 
 GR_MBUF_PRIV_DATA_TYPE(ip_output_mbuf_data, {
-	const struct iface *input_iface;
 	struct nexthop *nh;
 });
 
--- modules/ip6/datapath/gr_ip6_datapath.h
+++ modules/ip6/datapath/gr_ip6_datapath.h
@@ -18,7 +18,6 @@
 #include <stdint.h>
 
 GR_MBUF_PRIV_DATA_TYPE(ip6_output_mbuf_data, {
-	const struct iface *input_iface;
 	struct nexthop6 *nh;
 });
 
@@ -33,7 +32,6 @@ GR_MBUF_PRIV_DATA_TYPE(ip6_local_mbuf_data, {
 	uint16_t len;
 	uint8_t hop_limit;
 	uint8_t proto;
-	const struct iface *input_iface;
 });
 
 void ip6_input_local_add_proto(uint8_t proto, const char *next_node);
