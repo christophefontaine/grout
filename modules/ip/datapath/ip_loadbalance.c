f8ec9bb43dacdec9c325954c1dfe1e039b4237b0
Christophe Fontaine <cfontain@redhat.com>
2025-10-28 11:30:23 +0100

    ip: add xstats
    Now that xstats are fixed upstream, use them to log when
    a RSS is invalid.
    
    Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

--- modules/ip/datapath/ip_loadbalance.c
+++ modules/ip/datapath/ip_loadbalance.c
@@ -17,6 +17,11 @@ enum edges {
 	EDGE_COUNT,
 };
 
+enum xstats {
+	NO_RSS,
+	XSTATS_COUNT,
+};
+
 static uint16_t ip_loadbalance_process(
 	struct rte_graph *graph,
 	struct rte_node *node,
@@ -38,7 +43,8 @@ static uint16_t ip_loadbalance_process(
 			edge = NO_NEXTHOP;
 			goto next;
 		}
-		// TODO: increment xstat on ! mbuf->ol_flags & RTE_MBUF_F_RX_RSS_HASH
+		if (unlikely((mbuf->ol_flags & RTE_MBUF_F_RX_RSS_HASH) == 0))
+			rte_node_xstat_increment(node, NO_RSS, 1);
 		d->nh = g->members[mbuf->hash.rss % g->n_members].nh;
 next:
 		if (gr_mbuf_is_traced(mbuf))
@@ -54,6 +60,13 @@ static void loadbalance_register(void) {
 	ip_output_register_nexthop_type(GR_NH_T_GROUP, "ip_loadbalance");
 }
 
+static struct rte_node_xstats ip_loadbalance_xstats = {
+        .nb_xstats = XSTATS_COUNT,
+        .xstat_desc = {
+                [NO_RSS] = "no_valid_rss",
+        },
+};
+
 static struct rte_node_register ip_lb_node = {
 	.name = "ip_loadbalance",
 	.process = ip_loadbalance_process,
@@ -62,6 +75,7 @@ static struct rte_node_register ip_lb_node = {
 		[OUTPUT] = "ip_output",
 		[NO_NEXTHOP] = "ip_lb_no_nexthop",
 	},
+	.xstats = &ip_loadbalance_xstats,
 };
 
 static struct gr_node_info info_loadbalance = {
--- modules/ip6/datapath/ip6_loadbalance.c
+++ modules/ip6/datapath/ip6_loadbalance.c
@@ -17,6 +17,11 @@ enum edges {
 	EDGE_COUNT,
 };
 
+enum xstats {
+	NO_RSS,
+	XSTATS_COUNT,
+};
+
 static uint16_t ip6_loadbalance_process(
 	struct rte_graph *graph,
 	struct rte_node *node,
@@ -38,7 +43,8 @@ static uint16_t ip6_loadbalance_process(
 			edge = NO_NEXTHOP;
 			goto next;
 		}
-		// TODO: increment xstat on ! mbuf->ol_flags & RTE_MBUF_F_RX_RSS_HASH
+		if (unlikely((mbuf->ol_flags & RTE_MBUF_F_RX_RSS_HASH) == 0))
+			rte_node_xstat_increment(node, NO_RSS, 1);
 		d->nh = g->members[mbuf->hash.rss % g->n_members].nh;
 next:
 		if (gr_mbuf_is_traced(mbuf))
@@ -54,6 +60,13 @@ static void loadbalance_register(void) {
 	ip6_output_register_nexthop_type(GR_NH_T_GROUP, "ip6_loadbalance");
 }
 
+static struct rte_node_xstats ip6_loadbalance_xstats = {
+        .nb_xstats = XSTATS_COUNT,
+        .xstat_desc = {
+                [NO_RSS] = "no_valid_rss",
+        },
+};
+
 static struct rte_node_register ip6_lb_node = {
 	.name = "ip6_loadbalance",
 	.process = ip6_loadbalance_process,
@@ -62,6 +75,7 @@ static struct rte_node_register ip6_lb_node = {
 		[OUTPUT] = "ip6_output",
 		[NO_NEXTHOP] = "ip6_lb_no_nexthop",
 	},
+	.xstats = &ip6_loadbalance_xstats,
 };
 
 static struct gr_node_info info_loadbalance = {
