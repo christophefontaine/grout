fbca38b70b9823000543732f4c3511a79afb6ecf
Christophe Fontaine <cfontain@redhat.com>
2026-07-22 09:11:37 +0200

    Revert "fdb: fix missing mac in hw when an entry is updated"
    This reverts commit 3f644766376232bfabe2db5f2dd40a956bdd7384.

--- modules/l2/control/fdb.c
+++ modules/l2/control/fdb.c
@@ -256,8 +256,6 @@ static inline bool fdb_match(
 		return false;
 	if ((flags & GR_FDB_F_EXTERN) && !(e->flags & GR_FDB_F_EXTERN))
 		return false;
-	if ((flags & GR_FDB_F_HW) && !(e->flags & GR_FDB_F_HW))
-		return false;
 	if (bridge_id != GR_IFACE_ID_UNDEF && e->bridge_id != bridge_id)
 		return false;
 	if (iface_id != GR_IFACE_ID_UNDEF && e->iface_id != iface_id)
@@ -354,8 +352,7 @@ static void push_mac_to_hw(struct iface *iface, const struct rte_ether_addr *mac
 
 static void fdb_event_cb(uint32_t event, const void *obj) {
 	const struct iface_info_bridge *bridge_info;
-	struct gr_fdb_entry *fdb = (void *)obj;
-	bool add = event != GR_EVENT_FDB_DEL;
+	const struct gr_fdb_entry *fdb = obj;
 	const struct iface *bridge;
 
 	bridge = iface_from_id(fdb->bridge_id);
@@ -366,10 +363,6 @@ static void fdb_event_cb(uint32_t event, const void *obj) {
 
 	if ((fdb->flags & GR_FDB_F_EXTERN) == 0)
 		return;
-	if (add && (fdb->flags & GR_FDB_F_HW))
-		return;
-	if (!add && !(fdb->flags & GR_FDB_F_HW))
-		return;
 
 	bridge_info = iface_info_bridge(bridge);
 	for (unsigned i = 0; i < bridge_info->n_members; i++) {
@@ -377,13 +370,11 @@ static void fdb_event_cb(uint32_t event, const void *obj) {
 
 		// we have no clear idea what to do with a vlan_id if one got pushed by FRR
 		assert(fdb->vlan_id == 0);
-		push_mac_to_hw(member, &fdb->mac, add);
+		// FIXME: ideally, we should ask for adding the mac in hw only when getting the
+		// *first* transition from !EXTERN to EXTERN to handle both learning mac on the
+		// wire and from FRR.
+		push_mac_to_hw(member, &fdb->mac, event != GR_EVENT_FDB_DEL);
 	}
-
-	if (add)
-		fdb->flags |= GR_FDB_F_HW;
-	else
-		fdb->flags &= ~GR_FDB_F_HW;
 }
 
 void fdb_sync_hardware(const struct iface *bridge, struct iface *member, bool add) {
@@ -481,7 +472,6 @@ RTE_INIT(init) {
 	api_handler(GR_FDB_CONFIG_GET, fdb_config_get);
 	api_handler(GR_FDB_CONFIG_SET, fdb_config_set);
 	event_subscribe(GR_EVENT_FDB_ADD, fdb_event_cb);
-	event_subscribe(GR_EVENT_FDB_UPDATE, fdb_event_cb);
 	event_subscribe(GR_EVENT_FDB_DEL, fdb_event_cb);
 	event_serializer(GR_EVENT_FDB_ADD, NULL);
 	event_serializer(GR_EVENT_FDB_DEL, NULL);
